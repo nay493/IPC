6e734f13f5ed090f18aa9d939ff768d5c4b45231
Mohammed Nayeem <mohammednayeem45@gmail.com>
2018-02-19 19:17:13 +0530

    mmap.c: Add fstat.

--- ipc/mmap/mmap.c
+++ ipc/mmap/mmap.c
@@ -2,10 +2,20 @@
 #include<stdlib.h> // for exit()
 #include<sys/mman.h> // for mmap()
 #include <string.h> // for strcpy()
+// for fstat
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+// for fstat
+
+
 int main()
 {
-FILE *fd;
+//FILE *fd;
+int fd;
 void *memptr;
+struct stat *stat_ptr;
+int fstatd;
 
 fd=fopen("test.txt","r+");
 if(fd == NULL)
@@ -15,15 +25,21 @@ exit(255);
 }
 printf("file opened success\n");
 
-memptr = mmap(NULL,100,PROT_WRITE|PROT_READ,MAP_SHARED,fd,NULL);
+if(fstatd=fstat(fd,stat_ptr)==0)
+{
+printf("fstat.size=%lu\n",stat_ptr->st_size);
+}
+
+
+memptr = mmap(NULL,stat_ptr->st_size,PROT_WRITE|PROT_READ,MAP_SHARED,fd,NULL);
 if (memptr == MAP_FAILED)
 {
   printf("mem map failed!!\n");
 }
-printf("memmap success %x\n",memptr);
+printf("memmap success %x\n",(int)memptr);
 
 //*memptr="chal";
-strncpy(memptr,"CHAL HAT",8);
+strcpy(memptr,"CHALHAT");
 
 return 0;
 }
