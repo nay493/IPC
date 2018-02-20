4e19ea66059fe3c6df0464073ea53ed0435f0ca5
Mohammed Nayeem <mohammednayeem45@gmail.com>
2018-02-20 09:46:18 +0530

    signal.c: Added sigaction without handler.

--- ipc/signals/signals.c
+++ ipc/signals/signals.c
@@ -1,9 +1,27 @@
 #include<stdio.h>
+#include<signal.h> // for sigaction()
+
+/*
+ * command to find where gcc looks for headers:
+ * $ `gcc -print-prog-name=cc1` -v
+ *
+ *
+ * */
+
 
 int main()
 {
-int *p=NULL;
-
+int *p=NULL,ret;
+struct sigaction sa;
+ret=sigaction(SIGSEGV, &sa, NULL);
+if(ret == 0)
+{
+printf("sigaction success\n");
+}
+else
+{
+printf("sigaction failed!!\n");
+}
 *p=25;
 
 return 0;
